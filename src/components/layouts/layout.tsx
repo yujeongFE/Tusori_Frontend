7828f09282357d118996d16b8aa642a49565f8cf
MiraeKim <hufs202000512@gmail.com>
2024-02-17 01:42:54 +0900

    feat: 헤더 구현

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,12 +1,19 @@
+import React from "react";
+import styled from "styled-components";
 import Header from "./Header";
+
+const LayoutContainer = styled.div`
+    background: #FEFDFD;
+    padding: 0 9%;
+`;
 
 const Layout = (props: {children:React.ReactNode}) => {
     return(
     <>
         <Header />
-        <main>
+        <LayoutContainer>
             {props.children}
-        </main>
+        </LayoutContainer>
     </>
     );
 }
