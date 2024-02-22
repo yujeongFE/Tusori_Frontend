7caf1bbe4585e5a0c4d6ba1f98bbb26d51b5636d
MiraeKim <hufs202000512@gmail.com>
2024-02-22 23:38:23 +0900

    feat: 로그인 화면 초기 세팅

--- src/pages/Auth/Index.tsx
+++ src/pages/Auth/Index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Component } from "react";
 import Layout from "../../components/layouts/layout";
+import { LoginText } from "./Style";
 
 const Index = () => {
     return (
-      <Layout>
-        <h4>로그인 화면</h4>
-      </Layout>
+        <Layout>
+            <LoginText>로그인</LoginText>
+        </Layout>
     )
-  }
+}
   
   export default Index;
   
