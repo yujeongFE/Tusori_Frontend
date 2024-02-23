72d66b4f7735e91d1fa153d94178b092f973e405
MiraeKim <hufs202000512@gmail.com>
2024-02-23 21:04:45 +0900

    feat: 카카오 로그인 UI

--- src/pages/Auth/Index.tsx
+++ src/pages/Auth/Index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../../components/layouts/layout";
-import { Center, LoginText } from "./Style";
+import { LoginText, Center, Text, KakaoBtn, CautionText } from "./Style";
 
 const Index = () => {
     return (
@@ -8,7 +8,9 @@ const Index = () => {
             <LoginText>로그인</LoginText>
             <Center>
                 <img src={`${process.env.PUBLIC_URL}/assets/Tiger.webp`} alt="login" style={{ width: "130px", marginTop: "83px"}}/>
-                
+                <Text>'모의주식'으로 주식의<br/> 기본 과정을 알아가 볼까요?</Text>
+                <KakaoBtn>카카오 로그인/회원가입</KakaoBtn>
+                <CautionText>회원가입 시 모의주식의 서비스 이용 약관과 개인정보 보호정책에 동의하게 됩니다.</CautionText>
             </Center>
             
         </Layout>
@@ -18,3 +20,4 @@ const Index = () => {
   
   export default Index;
   
+  /* caution text bold */
--- src/pages/Auth/Style.tsx
+++ src/pages/Auth/Style.tsx
@@ -13,3 +13,29 @@ export const Center = styled.div`
     align-items: center;
     flex-direction: column;
 `;
+
+export const Text = styled.div`
+    text-align: center;
+    font-family: Pretendard-Medium;
+    font-size: 22px;
+    margin-top: 19px;
+`
+
+export const KakaoBtn = styled.button`
+    width: 483px;
+    height: 53px;
+    border-radius: 12px;
+    background: #FEE500;
+    margin-top: 40px;
+    border: none;
+    font-size: 18px;
+    font-family: Roboto-Regular;
+    cursor: pointer;
+`;
+
+export const CautionText = styled.div`
+    color: #B0B0B0;
+    font-size: 14px;
+    font-weight: 400;
+    margin-top: 32px;
+`;
