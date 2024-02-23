34b579e9983926e8758e239c43ef838a48b347f8
MiraeKim <hufs202000512@gmail.com>
2024-02-23 22:04:08 +0900

    feat: 카카오 로그인 화면 구현

--- src/pages/Auth/Index.tsx
+++ src/pages/Auth/Index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
                 <img src={`${process.env.PUBLIC_URL}/assets/Tiger.webp`} alt="login" style={{ width: "130px", marginTop: "83px"}}/>
                 <Text>'모의주식'으로 주식의<br/> 기본 과정을 알아가 볼까요?</Text>
                 <KakaoBtn>카카오 로그인/회원가입</KakaoBtn>
-                <CautionText>회원가입 시 모의주식의 서비스 이용 약관과 개인정보 보호정책에 동의하게 됩니다.</CautionText>
+                <CautionText>회원가입 시 모의주식의 <b>서비스 이용 약관</b>과 <b>개인정보 보호정책</b>에 동의하게 됩니다.</CautionText>
             </Center>
             
         </Layout>
