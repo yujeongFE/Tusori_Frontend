8a624412ad34d409c77af319bade2a89a2a0f452
MiraeKim <hufs202000512@gmail.com>
2024-02-29 22:25:46 +0900

    feat: 주식 투자 과정 버튼 컴포넌트 구현

--- src/pages/Dictionary/Process/Index.tsx
+++ src/pages/Dictionary/Process/Index.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../../../components/SideBar";
+//import styled from "styled-components";
+import ProcessContent from "../../../components/ProcessContent";
 import { SidebarContainer, Container, Content, Title } from "../../Dictionary/Style";
 
-const Index = () => {
+interface ContentItem {
+  number: string;
+  content: string;
+}
+
+const Index: React.FC = () => {
+  const [numberKey, setNumberKey] = useState<string>();
+
+  const handleClickButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { name } = e.currentTarget;
+    setNumberKey(name);
+  };
+
   return (
     <Container>
       <SidebarContainer>
@@ -10,6 +24,28 @@ const Index = () => {
       </SidebarContainer>
       <Content>
         <Title>주식 투자 과정</Title>
+        <button onClick={handleClickButton} name="one">
+          Page 1
+        </button>
+        <button onClick={handleClickButton} name="two">
+          Page 2
+        </button>
+        <button onClick={handleClickButton} name="three">
+          Page 3
+        </button>
+        <button onClick={handleClickButton} name="four">
+          Page 4
+        </button>
+        <button onClick={handleClickButton} name="five">
+          Page 5
+        </button>
+        <button onClick={handleClickButton} name="six">
+          Page 6
+        </button>
+        <button onClick={handleClickButton} name="seven">
+          Page 7
+        </button>
+        {numberKey && <ProcessContent numberKey={numberKey} />}
       </Content>
     </Container>
   );
--- src/pages/Dictionary/Words/Index.tsx
+++ src/pages/Dictionary/Words/Index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import SideBar from "../../../components/SideBar";
 import { SidebarContainer, Container, Content, Title } from "../../Dictionary/Style";
 
-const Index = () => {
+const Index: React.FC = () => {
   return (
     <Container>
       <SidebarContainer>
