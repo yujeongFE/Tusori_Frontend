e9b125d3b6ad53f8943750d739cf7993127e7028
MiraeKim <hufs202000512@gmail.com>
2024-03-02 02:44:18 +0900

    feat: 마이페이지 구현

---
+++ src/pages/Mypage/Index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { MyInfoContainer, MyInfoBox} from "./Style";
+
+const Index: React.FC = () => {
+  return (
+    <>
+      <MyInfoContainer>
+        <MyInfoBox>
+        </MyInfoBox>
+      </MyInfoContainer>
+    </>
+  );
+};
+
+export default Index;
