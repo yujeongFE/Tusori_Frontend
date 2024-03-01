669ed5a478041271e9da04bb9efcfa93ccb6e2d8
MiraeKim <hufs202000512@gmail.com>
2024-03-02 02:45:27 +0900

    refactor: ts 컴포넌트 타입 명시

--- src/pages/Auth/Index.tsx
+++ src/pages/Auth/Index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { LoginText, Center, Text, KakaoBtn, CautionText } from "./Style";
 
-const Index = () => {
+const Index: React.FC = () => {
   return (
     <>
       <LoginText>로그인</LoginText>
