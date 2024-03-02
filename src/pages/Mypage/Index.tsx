3fd1ac3c29dbb18cc5ed779586c5acb449ec28fd
MiraeKim <hufs202000512@gmail.com>
2024-03-02 22:32:10 +0900

    feat: 마이페이지 기본 구조

--- src/pages/Mypage/Index.tsx
+++ src/pages/Mypage/Index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Profile from "../../components/Profile";
+import MypageContainer from "components/MypageContainer";
+import { Container, Text } from "./Style";
 
 const Index: React.FC = () => {
   return (
-    <>
+    <Container>
       <Profile />
-    </>
+      <Text>My 보유주식</Text>
+      <MypageContainer />
+      <Text>관심 주식</Text>
+      <MypageContainer />
+      <Text>매수 일지</Text>
+      <MypageContainer />
+    </Container>
   );
 };
 
--- src/pages/Mypage/Style.tsx
+++ src/pages/Mypage/Style.tsx
@@ -1,2 +1,17 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  color: #2a2a2a;
+  margin: 0 0 20px 0;
+  text-align: left;
+  width: 80%;
+`;
