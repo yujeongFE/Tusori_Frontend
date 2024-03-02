2e7b961daa3f123790ac68baf36a56785cadc585
MiraeKim <hufs202000512@gmail.com>
2024-03-02 22:32:34 +0900

    feat: 마이페이지 박스 컴포넌트

---
+++ src/components/MypageContainer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const BoxContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 304px;
+  border-radius: 8px;
+  border: 1px solid #e3e3e3;
+  margin-bottom: 70px;
+`;
+
+const MypageContainer: React.FC = () => {
+  return (
+    <>
+      <BoxContainer> </BoxContainer>
+    </>
+  );
+};
+
+export default MypageContainer;
