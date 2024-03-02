d84953a9deabfa0fb1873b485b0a57131fb5e57c
MiraeKim <hufs202000512@gmail.com>
2024-03-03 00:31:42 +0900

    feat: 마이페이지 테이블 기본 구조

--- src/components/MypageTable.tsx
+++ src/components/MypageTable.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
-const TableContainer = styled.div`
+type TableProps = {
+  headers: string[];
+  data: any[][];
+};
+
+type PaddingProps = {
+  isFirstColumn: boolean;
+  isLastColumn: boolean;
+};
+
+const MyPageTable = ({ headers, data }: TableProps) => (
+  <TableContainer>
+    <thead>
+      <tr>
+        {headers.map((header, index) => (
+          <Th key={index} isFirstColumn={index === 0} isLastColumn={index === -1}>
+            {header}
+          </Th>
+        ))}
+      </tr>
+    </thead>
+    <Line />
+    <tbody>
+      {data.map((row, rowIndex) => (
+        <tr key={rowIndex}>
+          {row.map((cell, cellIndex) => (
+            <Td key={cellIndex} isFirstColumn={cellIndex === 0} isLastColumn={cellIndex === -1}>
+              {cell}
+            </Td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </TableContainer>
+);
+
+const TableContainer = styled.table`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 80%;
@@ -12,12 +49,22 @@ const TableContainer = styled.div`
   margin-bottom: 70px;
 `;
 
-const MypageContainer: React.FC = () => {
-  return (
-    <>
-      <TableContainer> </TableContainer>
-    </>
-  );
-};
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #e3e3e3;
+`;
 
-export default MypageContainer;
+const Th = styled.th<PaddingProps>`
+  padding-left: 5vw;
+  ${({ isFirstColumn }) => isFirstColumn && `padding: 0 9vw 0 0;`}
+  ${({ isLastColumn }) => isLastColumn && `padding: 0 0 0 0;`}
+`;
+
+const Td = styled.td<PaddingProps>`
+  //padding: 0 81px 0 0;
+  ${({ isFirstColumn }) => isFirstColumn && `padding-right: 150px;`}
+  ${({ isLastColumn }) => isLastColumn && `padding: 0 0 0 0;`}
+`;
+
+export default MyPageTable;
--- src/pages/Mypage/Index.tsx
+++ src/pages/Mypage/Index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Profile from "../../components/Profile";
-import MypageContainer from "components/MypageTable";
+import MypageTable from "components/MypageTable";
 import { Container, Text } from "./Style";
+
+const MystocksHeaders = ["종목명", "매입가", "현재가", "평단가", "보유수량", "보유일", "평가손익금", "평가손익률"];
+const InterestedHeaders = ["종목명", "현재가", "전일비", "등락률", "시가", "고가", "저가", "거래량", "시가총액"];
+const BuyingLogsHeaders = ["종목명", "매수일자", "체결일자", "주문단가", "체결단가", "주문수량", "수익금", "수익률"];
+
+const MyStocksData = [["삼성전자", "80,000", "82,000", "81,000", "100", "2021.01.01", "2,000,000", "2.5%"]];
+const InterestedData = [["삼성전자", "82,000", "2,000", "2.5%", "80,000", "82,000", "80,000", "1,000,000", "1,000,000,000"]];
+const BuyingLogsData = [["삼성전자", "2021.01.01", "2021.01.01", "80,000", "82,000", "100", "2,000,000", "2.5%"]];
 
 const Index: React.FC = () => {
   return (
     <Container>
       <Profile />
       <Text>My 보유주식</Text>
-      <MypageContainer />
+      <MypageTable headers={MystocksHeaders} data={MyStocksData} />
       <Text>관심 주식</Text>
-      <MypageContainer />
+      <MypageTable headers={InterestedHeaders} data={InterestedData} />
       <Text>매수 일지</Text>
-      <MypageContainer />
+      <MypageTable headers={BuyingLogsHeaders} data={BuyingLogsData} />
     </Container>
   );
 };
