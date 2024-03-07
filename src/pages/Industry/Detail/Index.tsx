7d73c042d38c3594c7daf9c936c49c8d5d681395
yujeongFE <yujeong6231@naver.com>
2024-03-07 21:27:35 +0900

    feat: 종목 상세 페이지 더미 데이터 삽입

--- src/pages/Industry/Detail/Index.tsx
+++ src/pages/Industry/Detail/Index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import { FlexBox } from "./Style";
+import StockInfoTable from "components/StockInfoTable";
+import StockData from "../../../json/IndustryStockData.json";
 import IndustrySidebar from "components/IndustrySideBar";
 
 const Index = () => {
+  const titles = ["종목명", "현재가", "전일비", "등락률(%)", "거래량", "순매수호가잔량"];
+  const dataKeys = ["name", "currentPrice", "priceChange", "percentChange", "value", "bid"];
+
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
   const handleItemSelected = (item: string | null) => {
@@ -13,6 +18,7 @@ const Index = () => {
   return (
     <FlexBox>
       <IndustrySidebar onItemSelected={handleItemSelected} />
+      <StockInfoTable titles={titles} dataKeys={dataKeys} data={StockData} />
     </FlexBox>
   );
 };
