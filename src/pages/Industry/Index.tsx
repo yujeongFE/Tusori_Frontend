d05ac1957c442c638e6dc8c6ebca5ecd5b46e0fc
yujeongFE <yujeong6231@naver.com>
2024-03-07 15:14:03 +0900

    chore: 병합 시 충돌로 삭제된 파일을 복구

---
+++ src/pages/Industry/Index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FlexBox } from "./Style";
+import IndustrySectorBox from "components/IndustrySectorBox";
+
+const Index = () => {
+  return (
+    <FlexBox>
+      <IndustrySectorBox></IndustrySectorBox>
+    </FlexBox>
+  );
+};
+
+export default Index;
