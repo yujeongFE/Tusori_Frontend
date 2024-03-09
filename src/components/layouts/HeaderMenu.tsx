b9d0464d3f7dd715808badb3b3bff9e3fa130f2e
MiraeKim <hufs202000512@gmail.com>
2024-03-09 23:08:17 +0900

    feat: 사이드바 로그인관련 컴포넌트

--- src/components/layouts/HeaderMenu.tsx
+++ src/components/layouts/HeaderMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import { NavLink, useLocation } from "react-router-dom";
 
@@ -13,6 +13,13 @@ const activeLinkStyle = css`
     background-color: #708ffe;
     bottom: 0;
     left: 0;
+  }
+
+  // 768px 이하일때 after 없애기
+  @media (max-width: 768px) {
+    &:after {
+      content: none;
+    }
   }
 `;
 
@@ -34,27 +41,117 @@ const StyledNavLink = styled(NavLink)`
   }
 `;
 
-const SidebySideContainer = styled.div`
+const SidebySideContainer = styled.div<{ isOpen: boolean }>`
   display: flex;
-  flex-direction: space-between;
-  margin-right: 68px;
+  flex-direction: column;
+  width: 304px;
+  position: fixed;
+  height: 100%;
+  left: ${({ isOpen }) => (isOpen ? "0" : "-304px")};
+  top: 0;
+  background-color: #f8f9fa;
+  overflow-x: hidden;
+  transition: 0.5s;
+  z-index: 1;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    position: static;
+    width: auto;
+    height: auto;
+    background-color: transparent;
+  }
+
+  @media (max-width: 400px) {
+    width: 250px;
+    left: ${({ isOpen }) => (isOpen ? "0" : "-250px")};
+  }
+  @media (max-width: 300px) {
+    width: 200px;
+    left: ${({ isOpen }) => (isOpen ? "0" : "-250px")};
+  }
 `;
 
-const HeaderMenu = () => {
+const SidebarToggleButton = styled.button`
+  display: block;
+  position: fixed;
+  top: 15px;
+  left: 15px;
+  z-index: 2;
+  background: url("${process.env.PUBLIC_URL}/assets/menu_icon.svg") no-repeat center center;
+  background-size: contain;
+  border: none;
+  cursor: pointer;
+  padding: 12px;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const SidebarCloseButton = styled.button`
+  display: block;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  background: url("${process.env.PUBLIC_URL}/assets/close_icon.svg") no-repeat center center;
+  background-size: contain;
+  border: none;
+  cursor: pointer;
+  padding: 12px;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 88px 0 0 7.5%;
+  border-radius: 8px;
+  background: #eff3ff;
+  padding: 16px 0 16px 0;
+  width: 85%;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const HeaderMenu: React.FC = () => {
   const location = useLocation();
-  const checkActive = (path: string) => {
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
+
+  const toggleSidebar = (): void => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  const checkActive = (path: string): boolean => {
     return ["/dict/process", "/dict/words"].includes(path);
   };
 
   return (
-    <SidebySideContainer>
-      <StyledNavLink to="/">홈</StyledNavLink>
-      <StyledNavLink to="/dict/process" className={checkActive(location.pathname) ? "active" : ""}>
-        주식사전
-      </StyledNavLink>
-      <StyledNavLink to="/industry">업종별시세</StyledNavLink>
-      <StyledNavLink to="/mypage">마이페이지</StyledNavLink>
-    </SidebySideContainer>
+    <>
+      {!isSidebarOpen && <SidebarToggleButton onClick={toggleSidebar} />}
+      <SidebySideContainer isOpen={isSidebarOpen}>
+        {isSidebarOpen && <SidebarCloseButton onClick={toggleSidebar} />}
+        {isSidebarOpen && (
+          // 사이드바에만 보이는 부분
+          <LoginContainer>
+            <img src={`${process.env.PUBLIC_URL}/assets/before_login.svg`} alt="login" style={{ width: "24px", margin: "0 16px 0 12px" }} />
+            로그인을 해주세요
+          </LoginContainer>
+        )}
+
+        <StyledNavLink to="/">홈</StyledNavLink>
+        <StyledNavLink to="/dict/process" className={checkActive(location.pathname) ? "active" : ""}>
+          주식사전
+        </StyledNavLink>
+        <StyledNavLink to="/industry">업종별시세</StyledNavLink>
+        <StyledNavLink to="/mypage">마이페이지</StyledNavLink>
+      </SidebySideContainer>
+    </>
   );
 };
 
