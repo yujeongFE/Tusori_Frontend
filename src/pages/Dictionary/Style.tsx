ea06a516c3b472fdbd1ea69397ea599cf6d32bc3
MiraeKim <hufs202000512@gmail.com>
2024-03-14 21:58:12 +0900

    feat: 반응형 메뉴바 추가

--- src/pages/Dictionary/Style.tsx
+++ src/pages/Dictionary/Style.tsx
@@ -2,18 +2,29 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  padding: 0 13vw;
 `;
 
 export const SideMenuContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 200px;
+  padding-left: 13vw;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const Content = styled.div`
   flex-grow: 1;
+`;
+
+export const ContentPadding = styled.div`
+  padding-right: 13vw;
   padding-left: 2vw;
+  @media (max-width: 768px) {
+    padding: 0 12vw;
+  }
 `;
 
 export const Title = styled.div`
--- src/pages/Dictionary/Words/Index.tsx
+++ src/pages/Dictionary/Words/Index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import SideMenu from "components/SideMenu";
-import DictMenu from "components/DictMenu";
-import { SideMenuContainer, Container, Content, Title } from "../Style";
+import SideMenu from "components/DictionaryMenu/SideMenu";
+import DictMenu from "components/DictionaryMenu/DictMenu";
+import { SideMenuContainer, Container, Content, ContentPadding, Title } from "../Style";
 import { SearchBoxContainer, SearchBox, Input, SearchButton } from "./Style";
 
 const Index: React.FC = () => {
@@ -12,13 +12,15 @@ const Index: React.FC = () => {
       </SideMenuContainer>
       <Content>
         <DictMenu />
-        <Title>주식 용어 설명</Title>
-        <SearchBoxContainer>
-          <SearchBox>
-            <Input type="text" placeholder="키워드를 입력해주세요" />
-            <SearchButton type="submit"></SearchButton>
-          </SearchBox>
-        </SearchBoxContainer>
+        <ContentPadding>
+          <Title>주식 용어 설명</Title>
+          <SearchBoxContainer>
+            <SearchBox>
+              <Input type="text" placeholder="키워드를 입력해주세요" />
+              <SearchButton type="submit"></SearchButton>
+            </SearchBox>
+          </SearchBoxContainer>
+        </ContentPadding>
       </Content>
     </Container>
   );
