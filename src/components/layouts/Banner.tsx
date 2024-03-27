7a14ceeb176143844a30cdaf3d33f80726ef8836
yujeongFE <yujeong6231@naver.com>
2024-03-27 16:21:51 +0900

    feat: 홈 화면 배너 캐로셀 구현

--- src/components/layouts/Banner.tsx
+++ src/components/layouts/Banner.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import styled from "styled-components";
+
+const BannerContainer = styled.div`
+  width: 100%;
+  overflow: hidden;
+`;
 
 const BannerStyle = styled.div`
   display: flex;
@@ -33,17 +41,34 @@ const Mascot = styled.div`
 `;
 
 const Banner = () => {
+  const defaultImages = [`${process.env.PUBLIC_URL}/assets/auth/character.png`, `${process.env.PUBLIC_URL}/assets/banner/second_banner.png`];
+  const defaultTexts = ["주식 왕초보님들!\n주식 용어와 과정을\n함께 알아가 보아요~", "복잡한 주식 용어,\n 직접 거래하면서\n 이해하러 가기"];
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
-    <div>
-      <BannerStyle>
-        <BannerText>
-          주식 왕초보님들! {"\n"} 주식 용어와 과정을 {"\n"} 함께 알아가 보아요~
-        </BannerText>
-        <Mascot>
-          <img src={`${process.env.PUBLIC_URL}/assets/auth/character.png`} style={{ width: "10.4vw", height: "15.7vh" }} />
-        </Mascot>
-      </BannerStyle>
-    </div>
+    <BannerContainer>
+      <Slider {...settings}>
+        {defaultImages.map((image, idx) => (
+          <div key={idx}>
+            <BannerStyle>
+              <BannerText>{defaultTexts[idx]}</BannerText>
+              <Mascot>
+                <img src={image} style={{ width: "10.4vw", height: "15.7vh" }} alt="banner" />
+              </Mascot>
+            </BannerStyle>
+          </div>
+        ))}
+      </Slider>
+    </BannerContainer>
   );
 };
 
