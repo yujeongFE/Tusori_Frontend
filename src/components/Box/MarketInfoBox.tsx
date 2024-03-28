f6f3cd3d5604310955af84337daa09efb0d545eb
yujeongFE <yujeong6231@naver.com>
2024-03-28 16:12:18 +0900

    feat: 모바일 버전 시장 정보 박스 구현

--- src/components/Box/MarketInfoBox.tsx
+++ src/components/Box/MarketInfoBox.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import data from "../../json/MarketData.json";
 import rising from "../../assets/rising_arrow.svg";
 import downward from "../../assets/downward_arrow.svg";
+import Slider from "react-slick";
 
-// 중앙 정렬 스타일을 적용할 컨테이너
+const Container = styled.div`
+  @media (max-width: 768px) {
+    height: 10.36vh;
+    min-height: 111px;
+    margin-top: 2.6vh;
+
+    .slick-slide {
+      margin-right: 5px;
+    }
+
+    .slick-slide:first-child {
+      margin-right: 5px;
+    }
+
+    .slick-slide:last-child {
+      margin-right: -10px;
+    }
+  }
+`;
+
 const MarketInfoBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,8 +37,13 @@ const MarketInfoBox = styled.div`
   box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.08);
   overflow: hidden;
 
-  @media (max-width: 767px) {
-    /* 작은 화면일 때 스타일링 */
+  @media (max-width: 768px) {
+    height: 10.36vh;
+    min-height: 111px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(to bottom, #4c4c4c 29px, #ffffff 29px);
   }
 `;
 
@@ -31,9 +56,20 @@ const Title = styled.div`
   line-height: normal;
   margin-top: 9.5px;
   margin-left: 1vw;
+
+  @media (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: bold;
+    font-weight: 800;
+    line-height: normal;
+    margin-top: 7px;
+  }
 `;
 
-// 현재 수치 값
 const CurrentIndex = styled.div`
   color: var(--Main-Font, #2a2a2a);
   text-align: center;
@@ -41,13 +77,19 @@ const CurrentIndex = styled.div`
   font-size: 36px;
   font-style: normal;
   line-height: normal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   @media (max-width: 1150px) {
     font-size: 25px;
   }
+  @media (max-width: 768px) {
+    margin-top: 20px;
+    font-size: 18px;
+  }
 `;
 
-// 변경된 수치 값
 const ChangeIndex = styled.div`
   color: #f00;
   font-family: Pretendard-Medium;
@@ -57,18 +99,39 @@ const ChangeIndex = styled.div`
   line-height: normal;
   text-align: center;
   margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   @media (max-width: 1150px) {
     font-size: 18px;
+    margin-top: 20px;
+  }
+  @media (max-width: 768px) {
+    font-size: 14px;
+    margin-top: 1.2vh;
   }
 `;
 
-// 변경된 퍼센트 값
 const ChangePercent = styled.span`
   margin-left: 1.5vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    margin-left: 4.26vw;
+  }
 `;
 
-// 상하 변경 방향을 나타내는 화살표 및 변경된 수치 값 컴포넌트
+const ArrowImg = styled.img`
+  @media (max-width: 768px) {
+    width: 14px;
+    height: 14px;
+    margin-right: 1vw;
+  }
+`;
+
 const Arrow: React.FC<{ value: string; percent: string }> = ({ value, percent }) => {
   const isPositive = parseFloat(value) >= 0;
   const arrowImage = isPositive ? rising : downward;
@@ -76,22 +139,21 @@ const Arrow: React.FC<{ value: string; percent: string }> = ({ value, percent })
 
   return (
     <ChangeIndex style={{ color: changeColor }}>
-      <img src={arrowImage} alt={isPositive ? "Rising" : "Downward"} style={{ marginRight: "5px" }} />
+      <ArrowImg src={arrowImage} alt={isPositive ? "Rising" : "Downward"} />
       {value} <ChangePercent>{percent}</ChangePercent>
     </ChangeIndex>
   );
 };
 
 interface MarketInfoProps {
-  title: string; // title prop 추가
+  title: string;
   index: string;
   change: string;
   percent: string;
   style?: React.CSSProperties;
 }
 
-// 상자에 담긴 모든 정보들을 표시하는 컴포넌트
-const MarketInfo: React.FunctionComponent<MarketInfoProps> = ({ title, index, change, percent, style }) => {
+const MarketInfo: React.FC<MarketInfoProps> = ({ title, index, change, percent, style }) => {
   return (
     <MarketInfoBox style={style}>
       <Title>{title}</Title>
@@ -105,8 +167,21 @@ const MarketInfo: React.FunctionComponent<MarketInfoProps> = ({ title, index, ch
   );
 };
 
-// 최종적인 박스 UI를 나타내는 컴포넌트
 const MarketInfoBoxContainer: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const titles = ["코스피", "코스닥", "환율"];
   const boxes = titles.map((title, index) => (
     <MarketInfo
@@ -115,11 +190,32 @@ const MarketInfoBoxContainer: React.FC = () => {
       index={data.indices[index]}
       change={data.change[index]}
       percent={data.percent[index]}
-      style={{ marginLeft: index === 0 ? "0vw" : "1.3vw" }}
+      style={{ marginRight: "1.3vw" }} // 슬라이드 간 간격을 조절합니다.
     />
   ));
 
-  return <div style={{ display: "flex", marginTop: "36px", width: "73vw" }}>{boxes}</div>;
+  if (isMobile) {
+    const settings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      row: 1,
+    };
+
+    return (
+      <Container>
+        <Slider {...settings}>{boxes}</Slider>
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <div style={{ display: "flex", marginTop: "36px" }}>{boxes}</div>
+      </Container>
+    );
+  }
 };
 
 export default MarketInfoBoxContainer;
