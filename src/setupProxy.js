5543a80abee35415e18e8b81ed7d10ed3a041e9b
yujeongFE <yujeong6231@naver.com>
2024-03-28 23:49:58 +0900

    fix: 프록시 서버 설정 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "fastapi//",
+    "fastapi/",
     createProxyMiddleware({
-      target: "http://15.164.142.161/fastapi/home/", // 프록시 대상 주소
+      target: "http://15.164.142.161/fastapi/", // 프록시 대상 주소
       changeOrigin: true, // 이 값을 true로 설정하여 프록시 대상의 origin을 변경
       pathRewrite: {
-        api: "/api", // 경로 재작성
+        api: "fastapi/", // 경로 재작성
       },
       logLevel: "error", // 로그 레벨 설정
       secure: false, // HTTPS를 사용하고 있다면 true로 설정
