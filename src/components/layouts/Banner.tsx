c377e0b772523cf1b12b6ad6a66855a1aa6745c4
yujeongFE <yujeong6231@naver.com>
2024-03-28 16:20:23 +0900

    feat: 모바일 버전 광고 배너 구현

--- src/components/layouts/Banner.tsx
+++ src/components/layouts/Banner.tsx
@@ -6,6 +6,10 @@ import styled from "styled-components";
 
 const BannerContainer = styled.div`
   width: 29vw;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const BannerStyle = styled.div`
@@ -18,6 +22,13 @@ const BannerStyle = styled.div`
   border-radius: 8px;
   background: #eff3ff;
   margin-top: 36px;
+
+  @media (max-width: 768px) {
+    width: 90.4vw;
+    height: 7.1vh;
+    min-height: 76px;
+    margin-top: 32px;
+  }
 `;
 
 const BannerText = styled.div`
@@ -34,15 +45,37 @@ const BannerText = styled.div`
   @media screen and (max-width: 1150px) {
     font-size: 18px;
   }
+
+  @media (max-width: 768px) {
+    color: var(--Main-Font, #2a2a2a);
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
 `;
 
-const Mascot = styled.div`
-  margin-left: 2vw;
+const Mascot = styled.img`
+  margin-left: 2.3vw;
+  width: 10.4vw;
+  height: 15.7vh;
+
+  @media (max-width: 768px) {
+    display: absolute;
+    width: 18vw;
+    height: 4.3vh;
+    min-height: 42.7px;
+    min-width: 78px;
+    margin-left: 4vw;
+    right: 2.6vw;
+  }
 `;
 
 const Banner = () => {
   const defaultImages = [`${process.env.PUBLIC_URL}/assets/auth/character.png`, `${process.env.PUBLIC_URL}/assets/banner/second_banner.png`];
   const defaultTexts = ["주식 왕초보님들!\n주식 용어와 과정을\n함께 알아가 보아요~", "복잡한 주식 용어,\n 직접 거래하면서\n 이해하러 가기"];
+  const defaultTexts_mobile = ["주식 왕초보님들!\n주식 용어와 과정을 알아가 보아요!", "복잡한 주식 용어,\n 직접 거래하면서 이해하러 가기"];
 
   const settings = {
     dots: false,
@@ -60,10 +93,8 @@ const Banner = () => {
         {defaultImages.map((image, idx) => (
           <div key={idx}>
             <BannerStyle>
-              <BannerText>{defaultTexts[idx]}</BannerText>
-              <Mascot>
-                <img src={image} style={{ width: "10.4vw", height: "15.7vh" }} alt="banner" />
-              </Mascot>
+              <BannerText>{window.innerWidth <= 768 ? defaultTexts_mobile[idx] : defaultTexts[idx]}</BannerText>
+              <Mascot src={image} alt="banner" />
             </BannerStyle>
           </div>
         ))}
