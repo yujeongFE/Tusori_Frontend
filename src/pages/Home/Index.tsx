822ad635859f2708c590c6173553c8b641927564
yujeongFE <yujeong6231@naver.com>
2024-04-04 13:40:21 +0900

    feat: 홈 화면 관련 api 연결

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MarketInfoBoxContainer from "../../components/Box/MarketInfoBox";
 import Banner from "../../components/layouts/Banner";
 import StockInfoBox from "../../components/Box/StockInfoBox";
 import { useWords } from "../../components/SideBar/DictionarySideBar/WordsContext";
 import { TableContainer, FlexBox, RowFlexBox } from "./Style";
+import { fetchHomePageData } from "api/home/MarketInfo";
 
 const Index = () => {
   const { setWords } = useWords();
@@ -11,11 +12,32 @@ const Index = () => {
     setWords([{ word: "", description: "" }]);
   }, [setWords]);
 
+  const [marketData, setMarketData] = useState<{
+    kospi: { Close: number; Comp: number; Change: number };
+    kosdaq: { Close: number; Comp: number; Change: number };
+    usdkrw_data: { close_price: number; percentage_change: number; price_change: number };
+  } | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const data = await fetchHomePageData();
+      if (data) {
+        setMarketData(data);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <FlexBox>
         <RowFlexBox>
-          <MarketInfoBoxContainer />
+          <MarketInfoBoxContainer marketData={marketData} />
           <Banner />
         </RowFlexBox>
         <TableContainer>
