21da9d4c88df6f98bb09c8dbc8342505924c48cb
yujeongFE <yujeong6231@naver.com>
2024-04-04 13:46:32 +0900

    feat: 홈 화면 로그인 여부 확인

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -16,6 +16,9 @@ const Index = () => {
     kospi: { Close: number; Comp: number; Change: number };
     kosdaq: { Close: number; Comp: number; Change: number };
     usdkrw_data: { close_price: number; percentage_change: number; price_change: number };
+    top_5_kospi: { Name: string; Close: string; Changes: number; ChangesRatio: number; Volume: number }[];
+    top_5_kosdaq: { Name: string; Close: string; Changes: number; ChangesRatio: number; Volume: number }[];
+    top_5_konex: { Name: string; Close: string; Changes: number; ChangesRatio: number; Volume: number }[];
   } | null>(null);
 
   const fetchData = async () => {
@@ -33,16 +36,27 @@ const Index = () => {
     fetchData();
   }, []);
 
+  const accessToken = localStorage.getItem("accessToken");
+  const login = accessToken ? true : false;
+
   return (
     <>
       <FlexBox>
         <RowFlexBox>
-          <MarketInfoBoxContainer marketData={marketData} />
+          <MarketInfoBoxContainer marketData={{ kospi: marketData?.kospi, kosdaq: marketData?.kosdaq, usdkrw_data: marketData?.usdkrw_data }} />
           <Banner />
         </RowFlexBox>
         <TableContainer>
-          <StockInfoBox title={"실시간 거래량 TOP5"} category={["코스피", "코스닥"]} />
-          <StockInfoBox title={"MY 보유 주식"} login={false} />
+          <StockInfoBox
+            title={"실시간 거래량 TOP5"}
+            category={["코스피", "코스닥", "코넥스"]}
+            stockData={{
+              top_5_kospi: marketData?.top_5_kospi || [],
+              top_5_kosdaq: marketData?.top_5_kosdaq || [],
+              top_5_konex: marketData?.top_5_konex || [],
+            }}
+          />
+          <StockInfoBox title={"MY 보유 주식"} login={login} />
         </TableContainer>
       </FlexBox>
     </>
