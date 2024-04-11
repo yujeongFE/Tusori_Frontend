cf9adb5eeaeb83f8fd4d12f9d9721c5a2865d8fc
yujeongFE <yujeong6231@naver.com>
2024-04-11 19:00:37 +0900

    feat: 업종별 상세 api 데이터 적용

--- src/components/Table/StockInfoTable.tsx
+++ src/components/Table/StockInfoTable.tsx
@@ -25,9 +25,10 @@ const TableHeaderCell = styled.th`
   text-align: left;
   border: 1px solid #ddd;
 `;
+
 interface TableCellProps {
-  value: string;
-  isChangeCell: boolean;
+  value?: number | string;
+  isChangeCell?: boolean | null;
 }
 
 const TableCellContainer = styled.div`
@@ -38,7 +39,7 @@ const TableCellContainer = styled.div`
 const TableCell = styled.td<TableCellProps>`
   padding: 10px;
   border: 1px solid #ddd;
-  color: ${({ value, isChangeCell }) => (isChangeCell && parseFloat(value) > 0 ? "red" : isChangeCell && parseFloat(value) < 0 ? "blue" : "inherit")};
+  color: ${({ isChangeCell, value }) => (isChangeCell && typeof value === "number" ? (value > 0 ? "red" : "blue") : "inherit")};
 `;
 
 const ArrowIcon = styled.img`
@@ -47,13 +48,21 @@ const ArrowIcon = styled.img`
   margin: 0px;
 `;
 
-interface StockInfoTableProps {
-  titles: string[];
-  dataKeys: string[];
-  data: { [key: string]: string }[];
+interface StockInfo {
+  ChagesRatio: number;
+  Changes: number;
+  Close: string;
+  Code: string;
+  Name: string;
+  Volume: number;
 }
 
-const StockInfoTable: React.FC<StockInfoTableProps> = ({ titles, dataKeys, data }) => {
+interface StockInfoTableProps {
+  titles: string[];
+  data: StockInfo[];
+}
+
+const StockInfoTable: React.FC<StockInfoTableProps> = ({ titles, data }) => {
   const { isOpen } = useWords();
 
   return (
@@ -63,29 +72,40 @@ const StockInfoTable: React.FC<StockInfoTableProps> = ({ titles, dataKeys, data 
           <tr>
             {titles.map((key, index) => (
               <TableHeaderCell key={index}>
-                {index === 1 && isOpen && <NumberBtn number={1}/>}
-                {index === 2 && isOpen && <NumberBtn number={2}/>}
-                {index === 3 && isOpen && <NumberBtn number={3}/>}
-                {index === 4 && isOpen && <NumberBtn number={4}/>}
+                {index === 1 && isOpen && <NumberBtn number={1} />}
+                {index === 2 && isOpen && <NumberBtn number={2} />}
+                {index === 3 && isOpen && <NumberBtn number={3} />}
+                {index === 4 && isOpen && <NumberBtn number={4} />}
                 {key}
               </TableHeaderCell>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.map((rowData, rowIndex) => (
-            <tr key={rowIndex}>
-              {dataKeys.map((dataKey, cellIndex) => (
-                <TableCell key={cellIndex} value={rowData[dataKey]} isChangeCell={dataKey === "priceChange" || dataKey === "percentChange"}>
+          {data.map((rowData, rowIndex) => {
+            console.log(rowData.ChagesRatio);
+            return (
+              <tr key={rowIndex}>
+                <TableCell>{rowData.Name}</TableCell>
+                <TableCell>{rowData.Close}</TableCell>
+                <TableCell isChangeCell={true} value={rowData.Changes}>
                   <TableCellContainer>
-                    {dataKey === "priceChange" && parseFloat(rowData[dataKey]) > 0 && <ArrowIcon src={rise} alt="rising_arrow" />}
-                    {dataKey === "priceChange" && parseFloat(rowData[dataKey]) < 0 && <ArrowIcon src={downward} alt="down_arrow" />}
-                    {rowData[dataKey]}
+                    {rowData.Changes > 0 && <ArrowIcon src={rise} alt="rising_arrow" />}
+                    {rowData.Changes < 0 && <ArrowIcon src={downward} alt="down_arrow" />}
+                    {rowData.Changes}
                   </TableCellContainer>
                 </TableCell>
-              ))}
-            </tr>
-          ))}
+                <TableCell isChangeCell={true} value={rowData.ChagesRatio}>
+                  <TableCellContainer>
+                    {rowData.ChagesRatio > 0 && <ArrowIcon src={rise} alt="rising_arrow" />}
+                    {rowData.ChagesRatio < 0 && <ArrowIcon src={downward} alt="down_arrow" />}
+                    {rowData.ChagesRatio}
+                  </TableCellContainer>
+                </TableCell>
+                <TableCell>{rowData.Volume}</TableCell>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </TableContainer>
